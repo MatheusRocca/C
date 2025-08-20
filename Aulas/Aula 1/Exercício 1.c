2625e4c6c915185330b0133eadf1b52223c92d96
MatheusRocca <matheusdudurocca@gmail.com>
2025-08-20 11:45:16 -0300

    Update Exercício 1.c

--- Aulas/Aula 1/Exercício 1.c
+++ Aulas/Aula 1/Exercício 1.c
@@ -1,1 +1,21 @@
+//Crie um programa em C que leia o valor do salário mínimo e o valor do salário bruto de uma pessoa. Calcular e imprimir quantos salários mínimos ela ganha.
 
+#include <stdio.h>
+
+int main() {
+    float salarioMinimo;
+    float quantidadeSalarios;
+    float salarioBruto;
+
+    printf("Digite o valor do salário mínimo: R$ ");
+    scanf("%f", &salarioMinimo);
+
+    printf("Digite o valor do seu salário bruto: R$ ");
+    scanf("%f", &salarioBruto);
+
+    quantidadeSalarios = salarioBruto / salarioMinimo;
+
+    printf("Você recebe aproximadamente %.2f salários mínimos.", quantidadeSalarios);
+
+    return 0;
+}
