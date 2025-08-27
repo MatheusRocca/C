fd9aeb8c0af2045f6eaeff2111784442c9e853cb
MatheusRocca <matheusdudurocca@gmail.com>
2025-08-26 22:13:34 -0300

    Create Exercício 2.c

---
+++ Aulas/Aula 3/Exercício 2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int calculos(float a,float b,float c);
+int main(){
+    float a,b,c;
+    printf("Digite o valor do lado A:");
+    scanf("%f",&a);
+    printf("Digite o valor do lado B:");
+    scanf("%f",&b);
+    printf("Digite o valor do lado C:");
+    scanf("%f",&c);
+    calculos(a,b,c);
+    return 0;
+}
+int calculos(float a, float b, float c){
+    if(a < b + c && 
+       b < a + c && 
+       c < a + b) {
+        printf("Este triangulo existe);
+    }
+    else{
+        printf("Este triângulo não existe.");
+    }
+    return 0;
+}
